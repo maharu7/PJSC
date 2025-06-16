950bbf86a70665c15c44feea5326716267ff637b
MAHARU <hx6n-tjm@asahi-net.or.jp>
2025-06-16 20:58:48 +0900

    Initial release

--- branches/Ver1.10/speeduino/engineProtection.h
+++
@@ -1,9 +0,0 @@
-
-#define HARD_REV_FIXED    1
-#define HARD_REV_COOLANT  2
-
-byte checkEngineProtect(void);
-byte checkRevLimit(void);
-byte checkBoostLimit(void);
-byte checkOilPressureLimit(void);
-byte checkAFRLimit(void);
